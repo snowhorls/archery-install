a3ddbda1591d7f3a6afe71b6bda097fc6f2d3465
snowhorls <133886718+snowhorls@users.noreply.github.com>
2023-07-11 13:53:15 +0000

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -10,7 +10,7 @@ ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
 hwclock --systohc
 
 echo "creating locales"
-echo "en_US-UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
